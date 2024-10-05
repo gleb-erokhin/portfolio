93b78f00c59bf42c63fe73317d434c86b79d2ec3
gleb-erokhin <gleb.erokhin@gmail.com>
2024-10-05 06:02:08 +0300

    мобильная навигация завершена

--- projects/webcademy/epicTravel/js/main.js
+++ projects/webcademy/epicTravel/js/main.js
@@ -1,11 +1,21 @@
 // ищем кнопку
 const btn = document.querySelector('#btn-mobile-nav');
+// ищем картинку бургер меню
 const mobileBtnImg = document.querySelector('#mobile-nav-img');
+const mobileNav = document.querySelector('#mobile-nav');
+const header = document.querySelector('#header');
 
 // Добавляем событие клика
 btn.addEventListener('click', function(){
     // при нажатии на кнопку добавляем/убираем класс open
     btn.classList.toggle('open');
+    // после добавления класса open включается мобильная навигация
+    mobileNav.classList.toggle('open');
+    // при включении мобильного меню добавляем белый фон шапки
+    header.classList.toggle('header-white-bg');
+    // отключаем скрол шапки в мобильной навигации добавление доп. класса
+    document.body.classList.toggle('no-scroll');
+
     // проверяем если класс open есть у кнопки = меняем картинку,
     if (btn.classList.contains('open')) {
         mobileBtnImg.src = `./img/icons/close-mobile-nav.svg`
