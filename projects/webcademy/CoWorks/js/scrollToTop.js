b3e48941a518388bf631e7c86f88efa212020136
gleb-erokhin <gleb.erokhin@gmail.com>
2024-10-19 08:05:59 +0300

    кнопка возврата в начало страницы

---
+++ projects/webcademy/CoWorks/js/scrollToTop.js
@@ -0,0 +1,14 @@
+//находим кнопку прокрутка к верху страницы по ID
+const scrollTopBtn = document.querySelector('#scrollToTopBtn');
+
+// слушаем событие скрола на странице
+window.addEventListener('scroll', function() {
+    // если прокрутили страницу больеше чем высота экрана
+    console.log('window.scrollY', window.scrollY);
+    console.log('window.innerHeight', window.innerHeight);
+    if (window.scrollY > (window.innerHeight - 400)) {
+        scrollTopBtn.classList.add('top-link--visible');
+    } else {
+        scrollTopBtn.classList.remove('top-link--visible');
+    }
+})
