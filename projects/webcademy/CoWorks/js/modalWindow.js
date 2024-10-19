89fcbed6f2c7d666ac4849738782d26ed4530ed9
gleb-erokhin <gleb.erokhin@gmail.com>
2024-10-19 23:23:11 +0300

    зайкрытие модалки  через escape

--- projects/webcademy/CoWorks/js/modalWindow.js
+++ projects/webcademy/CoWorks/js/modalWindow.js
@@ -6,9 +6,9 @@ const modal = document.querySelector('.modal');
 const closeModalBtn = document.querySelector('#closeModal');
 const modalBody = document.querySelector('.modal__body');
 
-/** работаем над открытием модального окна */ 
-opeModalBtns.forEach(function(item) {
-    item.addEventListener('click', function() {
+/** работаем над открытием модального окна */
+opeModalBtns.forEach(function (item) {
+    item.addEventListener('click', function () {
         // открываем модалку добавляя к ней класс modal--open
         modal.classList.add('modal--open');
         // чтобы страница не крутилась во время открытия модалки
@@ -17,18 +17,26 @@ opeModalBtns.forEach(function(item) {
 });
 
 /** работаем над закрытием модального окна */
-closeModalBtn.addEventListener('click', function() {
+closeModalBtn.addEventListener('click', function () {
     modal.classList.remove('modal--open');
     document.body.classList.remove('no-scroll');
-})
+});
 
 /** Закрытие модалки по поле затемнению */
-modal.addEventListener('click', function() {
+modal.addEventListener('click', function () {
     modal.classList.remove('modal--open');
     document.body.classList.remove('no-scroll');
-})
+});
 
 /** Отключаем поднятие клика чтобы при нажатии внутри модалки она не закрывалась */
-modalBody.addEventListener('click', function(event) {
+modalBody.addEventListener('click', function (event) {
     event.stopPropagation();
-})
+});
+
+/** Закрываем модалку по кнопке ESC */
+document.addEventListener('keydown', function (event) {
+    if (event.key == 'Escape') {
+        modal.classList.remove('modal--open');
+        document.body.classList.remove('no-scroll');
+    }
+});
