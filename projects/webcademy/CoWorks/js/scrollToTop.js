6b49da03b8ad2130f465319d2fc2cbc22ffdfcb2
gleb-erokhin <gleb.erokhin@gmail.com>
2024-10-19 10:38:23 +0300

    мобильная навигации выполнена. по умолчанию скрыта

--- projects/webcademy/CoWorks/js/scrollToTop.js
+++ projects/webcademy/CoWorks/js/scrollToTop.js
@@ -4,8 +4,8 @@ const scrollTopBtn = document.querySelector('#scrollToTopBtn');
 // слушаем событие скрола на странице
 window.addEventListener('scroll', function() {
     // если прокрутили страницу больеше чем высота экрана
-    console.log('window.scrollY', window.scrollY);
-    console.log('window.innerHeight', window.innerHeight);
+    // console.log('window.scrollY', window.scrollY);
+    // console.log('window.innerHeight', window.innerHeight);
     if (window.scrollY > (window.innerHeight - 400)) {
         scrollTopBtn.classList.add('top-link--visible');
     } else {
