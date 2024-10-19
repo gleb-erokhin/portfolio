09fe5d0cd1478459c977b7a2ccdda3b820aaba42
gleb-erokhin <gleb.erokhin@gmail.com>
2024-10-19 23:00:59 +0300

    работа с модальным окном

---
+++ projects/webcademy/CoWorks/js/modalWindow.js
@@ -0,0 +1,33 @@
+// ищем кнопки для открытия модального окна по атрибуту data-action="modal"
+const opeModalBtns = document.querySelectorAll('[data-action="modal"]')
+// ищем главное окно модалки
+const modal = document.querySelector('.modal');
+// находим кнопку закрытия окна модалки, ищем по id
+const closeModalBtn = document.querySelector('#closeModal');
+const modalBody = document.querySelector('.modal__body');
+
+/** работаем над открытием модального окна */ 
+opeModalBtns.forEach(function(item) {
+    item.addEventListener('click', function() {
+        // открываем модалку добавляя к ней класс modal--open
+        modal.classList.add('modal--open');
+        // чтобы страница не крутилась во время открытия модалки
+        document.body.classList.add('no-scroll');
+    });
+});
+
+/** работаем над закрытием модального окна */
+closeModalBtn.addEventListener('click', function() {
+    modal.classList.remove('modal--open');
+    document.body.classList.remove('no-scroll');
+})
+
+/** Закрытие модалки по поле затемнению */
+modal.addEventListener('click', function() {
+    modal.classList.remove('modal--open');
+})
+
+/** Отключаем поднятие клика чтобы при нажатии внутри модалки она не закрывалась */
+modalBody.addEventListener('click', function(event) {
+    event.stopPropagation();
+})
