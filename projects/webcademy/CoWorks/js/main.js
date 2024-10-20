47f124e8ac67f09da124146f5f97c88ccd07035a
gleb-erokhin <gleb.erokhin@gmail.com>
2024-10-20 22:21:22 +0300

    мобильный слайдер выполнен

--- projects/webcademy/CoWorks/js/main.js
+++ projects/webcademy/CoWorks/js/main.js
@@ -13,3 +13,16 @@ const slider = tns({
     controlsContainer: '.locations__controls',
     nav: false
 });
+
+// ищем кнопки слайдеро в мобильной версии страницы, ищу внутри блока элемент с классом prev  next
+const prevBtn = document.querySelector('.locations__controls--mobile .prev');
+const nextBtn = document.querySelector('.locations__controls--mobile .next');
+
+// обращаемся к кнопкам и вешаем на событие кнопки действие
+prevBtn.addEventListener('click', function() {
+    // используем свойства самого слайдера goTo
+    slider.goTo('prev');
+});
+
+// сокращенная форма записи, в виду того что мы имеем одну строыку кода
+nextBtn.onclick = () => slider.goTo('next');
